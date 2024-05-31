f873e213a56b7fd2ad6e79782abcb4cbdd5361bc
sathishsadie <sadie26032004@gmail.com>
2024-05-31 18:02:08 +0530

    Adding webapp file

---
+++ app.py
@@ -0,0 +1,51 @@
+import streamlit as st
+import pickle
+import pandas as pd
+
+
+
+st.title('Used Car Price Prediction')
+
+datas=pd.read_csv('ss.csv')
+
+datas=datas.loc[:,['Year','Present_Price', 'Kms_Driven',
+'Fuel_Type', 'Seller_Type', 'Transmission', 'Owner']]
+lis=list()
+for i in datas.columns:
+    if(datas[i].dtype=='category' or datas[i].dtype=='object'):
+        a = st.selectbox(i.title(), options=datas[i].unique())
+    else:
+        if(len(datas[i].unique())<=6):
+            a = st.selectbox(i.title(), options=datas[i].unique())
+        else:
+            datas[i]=datas[i].astype(float)
+            a = st.number_input(i.title(),min_value=datas[i].min(),max_value=datas[i].max(),value=datas[i].median(),step=0.5)
+    lis.append(a)
+
+
+
+with open('randomforest.pkl', 'rb') as f:
+    rf_reg = pickle.load(f)
+
+with open('labelencoder.pkl','rb') as fi:
+    fuel_type,seller_type,transmission = pickle.load(fi)
+
+
+
+def preprocess(input):
+    input['Fuel_Type']=fuel_type.transform(input['Fuel_Type'])
+    input['Seller_Type']=seller_type.transform(input['Seller_Type'])
+    input['Transmission']=transmission.transform(input['Transmission'])
+    return input
+
+
+if st.button('Predict Price'):
+    ss=datas.columns
+    dic={}
+    for i in range(len(ss)):
+        dic[ss[i]]=[lis[i]]
+    dff=pd.DataFrame(dic)
+    dff=preprocess(dff)
+    prediction = rf_reg.predict(dff)
+
+    st.write(f"Prediction of the Enteered Car Price  is {prediction[0]:.2f}")
